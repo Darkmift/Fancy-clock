42522a9ce6fad7471ec59f49b585b8d4722f3cfe
dark_mift <darkmift@gmail.com>
2019-12-01 11:38:43 +0200

    changed time to utc

--- main.js
+++ main.js
@@ -6,16 +6,16 @@ function zero(num) {
 }
 function newTime(zone) {
 	var d = new Date();
-	$(`#${zone.city}`).html(zero(d.getHours() + zone.offset));
+	$(`#${zone.city}`).html(zero(d.getUTCHours() + zone.offset));
 	$('.m').html(zero(d.getMinutes()));
 	$('.s').html(zero(d.getSeconds()));
 }
 const zones = [
 	{ city: 'TLV', offset: 2 },
-	{ city: 'MI', offset: -7 },
-	{ city: 'TB', offset: 2 },
-	{ city: 'LA', offset: -10 },
-	{ city: 'CH', offset: -8 },
+	{ city: 'MI', offset: -5 },
+	{ city: 'TB', offset: 4 },
+	{ city: 'LA', offset: -8 },
+	{ city: 'PH', offset: -7 },
 ];
 
 setInterval(() => {
